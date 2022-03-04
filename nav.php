3ec2a2b802d317361ef4b45bab79f9d2d165655d
chwung <weichung35@gmail.com>
2022-03-05 06:15:30 +0800

    nav bar

---
+++ nav.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="nav.css">
+<div class="page">
+  <nav class="page__menu menu">
+    <ul class="menu__list r-list">
+      <li class="menu__group"><a href="betterLogin.php" class="menu__link r-link text-underlined">Option #1</a></li>
+      <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">Option #2</a></li>
+      <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">Option #3</a></li>
+    </ul>
+  </nav>
+</div>
